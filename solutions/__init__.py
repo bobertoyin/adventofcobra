73d10c2c0d18ad3a4f98a7a31e83b5a221e83d08
Robert Yin <bobertoyin@gmail.com>
2023-11-30 23:44:30 -0600

    2023 day 1

--- solutions/__init__.py
+++ solutions/__init__.py
@@ -1,4 +1,5 @@
 from .runner import *
 from .s2015 import *
+from .s2023 import *
 
 __all__ = ["r", "Part"]
---
+++ solutions/s2023.py
@@ -0,0 +1,40 @@
+from .runner import r, Part
+
+
+def calibration_sum(solution_input: str, spelled: bool) -> int:
+    digits = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+    total = 0
+    for line in solution_input.split("\n"):
+        numbers = []
+        position = 0
+        while position < len(line):
+            if line[position].isnumeric():
+                numbers.append(int(line[position]))
+            elif spelled:
+                for digit in digits:
+                    if line.startswith(digit, position):
+                        numbers.append(digits[digit])
+                        break
+            position += 1
+        total += numbers[0] * 10 + numbers[-1]
+    return total
+
+
+@r.solution(2023, 1, Part.A)
+def s_2023_1_a(solution_input: str) -> int:
+    return calibration_sum(solution_input, False)
+
+
+@r.solution(2023, 1, Part.B)
+def s_2023_1_b(solution_input: str) -> int:
+    return calibration_sum(solution_input, True)
